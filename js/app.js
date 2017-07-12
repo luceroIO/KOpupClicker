70eae0845fc113bc8876f049c257023eadfeaa72
lucerio <lucero.develops@gmail.com>
2017-07-12 19:39:20 -0400

    created an observable array to hold nicknames and create a function to add differnt click levels

--- js/app.js
+++ js/app.js
@@ -3,6 +3,24 @@ var ViewModel =function(){
 	this.name = ko.observable('Ace');
 	this.imgSrc = ko.observable('img/pup1.jpg');
 	this.imgAttribution = ko.observable('https://www.google.com');
+	this.nicknames = ko.observableArray(['Leon', 'Mija', 'heyYo']);
+
+	this.title = ko.computed(function(){
+		var title;
+		var clicks = this,clickCount();
+		if (clicks < 10) {
+			title ='Newborn';
+		} else if (clicks < 50) {
+			title = 'Infant';
+		}else if (clicks < 100) {
+			title = 'Child';
+		}else if (clicks < 150) {
+			title = 'Teen';
+		}else if{
+			title = 'Ninja';
+		}
+		return title;	
+	})
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
